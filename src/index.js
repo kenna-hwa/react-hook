ced76d6e04911f3cd4183d12193e97d2ddf3817a
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-05 17:45:59 +0900

    useState 코드 노마드코더식으로 변경

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
 function App(){
   //useEffect 기본
   const [count, setCount] = useState(0);
+  const incrementCount = () => setCount(count+1);
+  const decrementCount = () => setCount(count-1);
 
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button onClick={function(){setCount(count+1)}}>더하기</button>
-      <button onClick={()=>setCount(count-1)}>빼기</button>
+      <button onClick={incrementCount}>더하기</button>
+      <button onClick={decrementCount}>빼기</button>
 
     </div>
   )
