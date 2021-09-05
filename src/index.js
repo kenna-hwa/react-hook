8e27ef8047e338fa420a48c332b66ca46a6ddc93
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-05 19:11:01 +0900

    useInput 코드 완성

--- src/index.js
+++ src/index.js
@@ -1,59 +1,78 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-  //useEffect 기본
+//useInput
 
-// function App(){
-//   const [count, setCount] = useState(0);
-//   const incrementCount = () => setCount(count+1);
-//   const decrementCount = () => setCount(count-1);
-
-//   return (
-//     <div className="App">
-//       <h1>{count}</h1>
-//       <button onClick={incrementCount}>더하기</button>
-//       <button onClick={decrementCount}>빼기</button>
-
-//     </div>
-//   )
-// }
-
-//class 로 변경
-
-class AppClass extends React.Component {
-
-    state = {
-      count : 0
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target : { value }
+    } = event;
+  
+    let willUpdate = true;
+    if (typeof validator == "function"){
+      willUpdate = validator(value)
     }
-
-    render(){
-      const { count } = this.state;
-      return (
-        <div className="App">
-          <h1>{count}</h1>
-          <button onClick={this.incrementCount}>더하기</button>
-          <button onClick={this.decrementCount}>빼기</button>
-    
-        </div>
-      )
+    if(willUpdate){
+      setValue(value);
     }
-    incrementCount = () => this.setState(
-      state => {
-        return {
-          count : state.count + 1
-        }
-      }
-    )
-    decrementCount  = () => this.setState(
-      state => {
-        return {
-          count : state.count - 1
-        }
-      }
-    )
+  };
+  return { value, onChange }
 
 }
 
+//useEffect 기본
+
+function App(){
+
+  const maxLen = value => !value.includes("@");
+  const name = useInput("Mr.", maxLen)
+
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <input {...name} />
+    </div>
+  )
+}
+
+//class 로 변경
+
+// class AppClass extends React.Component {
+
+//     state = {
+//       count : 0
+//     }
+
+//     render(){
+//       const { count } = this.state;
+//       return (
+//         <div className="App">
+//           <h1>{count}</h1>
+//           <button onClick={this.incrementCount}>더하기</button>
+//           <button onClick={this.decrementCount}>빼기</button>
+    
+//         </div>
+//       )
+//     }
+//     incrementCount = () => this.setState(
+//       state => {
+//         return {
+//           count : state.count + 1
+//         }
+//       }
+//     )
+//     decrementCount  = () => this.setState(
+//       state => {
+//         return {
+//           count : state.count - 1
+//         }
+//       }
+//     )
+
+// }
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<AppClass/>, rootElement);
+ReactDOM.render(<App/>, rootElement);
 
