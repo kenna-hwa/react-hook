69b994681f1eda45a9d5aa0475fa5cb6766902f0
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-05 17:55:38 +0900

    useState class component로 변환해보기

--- src/index.js
+++ src/index.js
@@ -1,22 +1,59 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function App(){
   //useEffect 기본
-  const [count, setCount] = useState(0);
-  const incrementCount = () => setCount(count+1);
-  const decrementCount = () => setCount(count-1);
 
-  return (
-    <div className="App">
-      <h1>{count}</h1>
-      <button onClick={incrementCount}>더하기</button>
-      <button onClick={decrementCount}>빼기</button>
+// function App(){
+//   const [count, setCount] = useState(0);
+//   const incrementCount = () => setCount(count+1);
+//   const decrementCount = () => setCount(count-1);
 
-    </div>
-  )
+//   return (
+//     <div className="App">
+//       <h1>{count}</h1>
+//       <button onClick={incrementCount}>더하기</button>
+//       <button onClick={decrementCount}>빼기</button>
+
+//     </div>
+//   )
+// }
+
+//class 로 변경
+
+class AppClass extends React.Component {
+
+    state = {
+      count : 0
+    }
+
+    render(){
+      const { count } = this.state;
+      return (
+        <div className="App">
+          <h1>{count}</h1>
+          <button onClick={this.incrementCount}>더하기</button>
+          <button onClick={this.decrementCount}>빼기</button>
+    
+        </div>
+      )
+    }
+    incrementCount = () => this.setState(
+      state => {
+        return {
+          count : state.count + 1
+        }
+      }
+    )
+    decrementCount  = () => this.setState(
+      state => {
+        return {
+          count : state.count - 1
+        }
+      }
+    )
+
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App/>, rootElement);
+ReactDOM.render(<AppClass/>, rootElement);
 
