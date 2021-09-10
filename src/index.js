9ab93ab6367c26ef91a72fc631c669095f1fbe62
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-10 13:19:57 +0900

    useState 연습 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 //import UseState from './useState';
-import App from './useInput';
+// import App from './useInput';
+import App from './useState2';
+
 
 
 
